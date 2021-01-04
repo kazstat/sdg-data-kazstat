4f9c04d7b7bffe5fc256efa00e3e26d12cf05612
Brock Fanning <brockfanning@gmail.com>
2021-01-04 12:45:24 -0500

    Simplify metadata

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,10 +1,4 @@
 from sdg.open_sdg import open_sdg_build
-
-def alter_meta(metadata):
-    indicator_id = metadata['indicator_number']
-    id_parts = indicator_id.split('.')
-    is_global_indicator = len(id_parts) == 3
-    metadata['is_global_indicator'] = is_global_indicator
-    return metadata
+from alterations import alter_meta
 
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,7 +1,8 @@
 from sdg.open_sdg import open_sdg_check
+from alterations import alter_meta
 
 # Validate the indicators.
-validation_successful = open_sdg_check(config='config_data.yml')
+validation_successful = open_sdg_check(config='config_data.yml', alter_meta=alter_meta)
 
 # If everything was valid, perform the build.
 if not validation_successful:
