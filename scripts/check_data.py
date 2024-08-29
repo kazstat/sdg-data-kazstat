2759e24d239399654122853054ca2f1307fe23fc
sarah-beasley <106670682+sarah-beasley@users.noreply.github.com>
2024-08-29 10:09:53 +0100

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -2,6 +2,11 @@ from sdg.open_sdg import open_sdg_check
 from alterations import alter_meta
 from alterations import alter_data
 
+def alter_data(df):
+  if "REF_AREA" in df:
+    df["GeoCode"]=df["REF_AREA"]
+  return df
+
 # Validate the indicators.
 validation_successful = open_sdg_check(config='config_data.yml', alter_meta=alter_meta, alter_data=alter_data)
 
