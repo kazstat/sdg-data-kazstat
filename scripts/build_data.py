4f4afadfe9f9bcf8323913c38d0d5ab96f9d2bc6
sarah-beasley <106670682+sarah-beasley@users.noreply.github.com>
2024-08-29 10:08:58 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -4,4 +4,9 @@ from alterations import alter_data
 import sdg
 import os
 
+def alter_data(df):
+  if "REF_AREA" in df:
+    df["GeoCode"]=df["REF_AREA"]
+  return df
+
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta, alter_data=alter_data)
